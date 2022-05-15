edc3a16077d4565b4c56ccffc01e8efe884c6af7
unknown <coolshiv0721@gmail.com>
2022-05-15 10:39:26 +0530

    Installed react-tsparticles, tsparticles, tsparticles-engine and imported in App.js, created particles.json(imported the same in App.js)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import Rank from './components/Rank/Rank';
+import particleOptions from './particles.json';
+import './App.css';
 
 function App() {
+  const particlesInit = useCallback((main) => {
+    loadFull(main);
+  }, []);
   return (
     <div className='App'>
+      <Particles options={particleOptions} init={particlesInit} />
       <Navigation />
       <Logo />
       <Rank />
