82131bd3b42448695c2eaf48890c17aa3de61e28
unknown <coolshiv0721@gmail.com>
2022-05-16 09:43:18 +0530

    Added Image Recognition API and fixed a million errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
+import Clarifai from 'clarifai';
 
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import particleOptions from './particles.json';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_API_KEY,
+});
+
 function App() {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
+      function(response) {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bouding_box
+        );
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  };
   const particlesInit = useCallback((main) => {
     loadFull(main);
   }, []);
@@ -19,8 +43,11 @@ function App() {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
+      <FaceRecognition imageUrl={imageUrl} />
     </div>
   );
 }
